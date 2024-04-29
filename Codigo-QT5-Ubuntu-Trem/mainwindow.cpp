da08b1b352f359ce2a190ea7ab222698e74335fc
julianapequeno <julianafpsc@gmail.com>
2024-04-29 11:22:07 -0300

    feat: molding trail's interface

--- Codigo-QT5-Ubuntu-Trem/mainwindow.cpp
+++ Codigo-QT5-Ubuntu-Trem/mainwindow.cpp
@@ -10,6 +10,7 @@ MainWindow::MainWindow(QWidget *parent) :
     //Cria o trem com seu (ID, posição X, posição Y)
     trem1 = new Trem(1,60,30);
     trem2 = new Trem(2,330,30);
+    //trem3 = new Trem(3,)
 
     /*
      * Conecta o sinal UPDATEGUI à função UPDATEINTERFACE.
